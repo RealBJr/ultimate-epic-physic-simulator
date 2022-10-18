83b8b8af034be1a60ac49b1b836b2cabd1e4f6c3
Junior Boni <2158914@edu.vaniercollege.qc.ca>
2022-10-18 13:02:33 -0400

    https://github.com/RealBJr/ultimate-epic-physic-simulator.git#

---
+++ ultimate-epic-physic-simulation/app/src/test/java/edu/vanier/ueps/graphs/Graphable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.vanier.ueps.graphs;
+
+/**
+ *
+ * @author 2158914
+ */
+public interface Graphable {
+    
+}
---
+++ ultimate-epic-physic-simulation/app/src/test/java/edu/vanier/ueps/simulations/Simulation.java
@@ -0,0 +1,7 @@
+
+package edu.vanier.ueps.simulations;
+
+
+public abstract class  Simulation {
+    
+}
