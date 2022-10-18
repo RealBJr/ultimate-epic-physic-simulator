3689e396724a1fd713ef24a5e5e0baf67788a1ce
Mijan Ullah <2163695@edu.vaniercollege.qc.ca>
2022-10-18 13:32:59 -0400

    added a test line

--- ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/ui/MainApp.java
+++ ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/ui/MainApp.java
@@ -10,5 +10,6 @@ public class MainApp {
 
     public static void main(String[] args) {
         System.out.println(new MainApp().getGreeting());
+        System.out.println("sub to leskayy");
     }
 }
