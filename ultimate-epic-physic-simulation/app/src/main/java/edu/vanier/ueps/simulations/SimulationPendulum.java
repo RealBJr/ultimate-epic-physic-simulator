b34e6fe50ee06b57c0822aa824364bdbe27ac9db
Junior Boni <2158914@edu.vaniercollege.qc.ca>
2022-10-25 13:54:52 -0400

    made simulations class extend simulation abstract class

--- ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/SimulationPendulum.java
+++ ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/SimulationPendulum.java
@@ -1,10 +1,40 @@
 
 package edu.vanier.ueps.simulations;
 
+import edu.vanier.ueps.simulations.controller.Controller;
+import java.io.File;
+import java.util.ArrayList;
+import javafx.scene.shape.Shape;
+
 /**
  *
  * @author 2158914
  */
-public class SimulationPendulum {
+public class SimulationPendulum extends Simulation{
+
+    @Override
+    public ArrayList<Shape> collisionDetect(Shape targetedShape, ArrayList<Shape> shapes) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void collisionReaction(Shape collidingShape) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void playSim(ArrayList<Shape> shapes) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void stopSim(ArrayList<Shape> shapes) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public File saveSim(Controller simController) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
