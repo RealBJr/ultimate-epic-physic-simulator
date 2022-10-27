98372426dcaef9f398cdcdf61aa804c37104b6ca
Junior Boni <2158914@edu.vaniercollege.qc.ca>
2022-10-27 10:35:39 -0400

    added a controller package in graphs package

--- ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/graphs/GraphGenerator.java
+++ ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/graphs/GraphGenerator.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package edu.vanier.ueps.graphs;
 
-/**
- *
- * @author 2158914
- */
 public class GraphGenerator {
     
 }
---
+++ ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/graphs/controllers/Controller.java
@@ -0,0 +1,8 @@
+
+package edu.vanier.ueps.graphs.controllers;
+
+import javafx.fxml.FXMLLoader;
+
+public class Controller {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/graphGenerator"));
+}
