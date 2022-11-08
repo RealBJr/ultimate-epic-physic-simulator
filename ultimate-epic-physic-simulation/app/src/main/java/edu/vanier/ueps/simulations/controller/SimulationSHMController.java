50deb590669d1c4116aac34a5c08026bbb77d9f3
Mijan Ullah <2163695@edu.vaniercollege.qc.ca>
2022-11-08 14:11:40 -0500

    Duration problem is fixed thanks to me

--- ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/controller/SimulationSHMController.java
+++ ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/controller/SimulationSHMController.java
@@ -26,101 +26,97 @@ import javafx.util.Duration;
  *
  * @author Youssif
  */
-public class SimulationSHMController implements Initializable{
+public class SimulationSHMController implements Initializable {
+
     @FXML
     Rectangle rect;
-    
+
     @FXML
     Node spring;
-    
+
     @FXML
     Button playbtn, stopbtn, pausebtn;
-    
+
     @FXML
     Slider frictionslider;
-    
+
     @FXML
-    ColorPicker colorPicker;        
-           
-    
-    
-    
-    Duration duration = Duration.seconds(2);
-    
+    ColorPicker colorPicker;
+
+    Duration originalDuration = Duration.seconds(2);
+    Duration duration;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //TODO: The spring extends on both sides, it should be only extending on the right hand side, to fix
-        
-        colorPicker.setOnAction((e)->{
-           Color newColor = colorPicker.getValue();
-           rect.setFill(newColor);
-       });
-        
-         frictionslider.valueProperty().addListener(new ChangeListener<Number>() {
-         public void changed(ObservableValue <?extends Number>observable, Number oldValue, Number newValue){
-            duration = Duration.seconds(duration.toSeconds()* frictionslider.getValue());
-         }
-      });
-        
-        
-        
-        
-        
-        
-        TranslateTransition tran = new TranslateTransition(duration, rect);
+
+        colorPicker.setOnAction((e) -> {
+            Color newColor = colorPicker.getValue();
+            rect.setFill(newColor);
+        });
+
+        frictionslider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (frictionslider.getValue() >= 1) {
+                    duration = Duration.seconds(originalDuration.toSeconds() * frictionslider.getValue());
+                    System.out.println(duration);
+
+                }
+                
+            }
+        });
+
+        TranslateTransition tran = new TranslateTransition(originalDuration, rect);
         tran.setByX(300f);
         tran.setCycleCount(Animation.INDEFINITE);
         tran.setAutoReverse(true);
         tran.setInterpolator(Interpolator.LINEAR);
-        
-        ScaleTransition tran2 = new ScaleTransition(duration, spring);
+
+        ScaleTransition tran2 = new ScaleTransition(originalDuration, spring);
         tran2.setByX(5.5);
-        
+
         tran2.setCycleCount(Animation.INDEFINITE);
         tran2.setAutoReverse(true);
         tran2.setInterpolator(Interpolator.LINEAR);
-        
-        System.out.println(tran.getDuration()+"here we are in initialize of controller");
-        
-        playbtn.setOnAction((e)->{
-            if(pausebtn.isDisable() == true){
-            tran.playFrom(Duration.ONE);
-            tran2.playFrom(Duration.ONE);
-            pausebtn.setDisable(false);
-            }else{
-            tran.play();
-            tran2.play();
+
+        System.out.println(tran.getDuration() + "here we are in initialize of controller");
+
+        playbtn.setOnAction((e) -> {
+            if (pausebtn.isDisable() == true) {
+                tran.playFrom(Duration.ONE);
+                tran2.playFrom(Duration.ONE);
+                pausebtn.setDisable(false);
+            } else {
+                tran.play();
+                tran2.play();
             }
             pausebtn.setDisable(false);
             frictionslider.setDisable(true);
         });
-        
-        pausebtn.setOnAction((e)->{
+
+        pausebtn.setOnAction((e) -> {
             tran.pause();
             tran2.pause();
         });
-        
-        stopbtn.setOnAction((e)->{
+
+        stopbtn.setOnAction((e) -> {
             tran.pause();
             tran2.pause();
             pausebtn.setDisable(true);
             frictionslider.setDisable(false);
         });
-        
-        frictionslider.valueProperty().addListener(new ChangeListener<Number>(){
-            
+
+        frictionslider.valueProperty().addListener(new ChangeListener<Number>() {
+
             int myfriction;
+
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 myfriction = (int) frictionslider.getValue();
                 //TODO: impliment the logic required to the animation when there is friction
-                
+
             }
         });
     }
-   
 
-   
-    
-    
 }
