dbf79a6154b2c994e04bbf3412be862b3379a7f5
RealBJr <2158914@edu.vaniercollege.qc.ca>
2022-11-26 23:56:12 -0500

    fixed clickAndDrag function

--- ultimate-epic-physic-simulation/ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/controller/SimulationSHMController.java
+++ ultimate-epic-physic-simulation/ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/controller/SimulationSHMController.java
@@ -6,12 +6,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
@@ -35,7 +34,7 @@ public class SimulationSHMController implements Initializable {
     Node spring;
 
     @FXML
-    Button playbtn, stopbtn, pausebtn, graphbtn;
+    Button playbtn, stopbtn, pausebtn, graphbtn, resetbtn;
 
     @FXML
     Slider frictionslider, AmplitudeSlider, PeriodSlider, PhaseSlider;
@@ -43,12 +42,46 @@ public class SimulationSHMController implements Initializable {
     @FXML
     ColorPicker colorPicker;
 
+    @FXML
+    Line linePathPositive, linePathNegative, lineMiddle;
+
     Duration originalDuration = Duration.seconds(2);
     Duration duration;
 
     double amplitude = 100;
 
-   
+   EventHandler<MouseEvent> eventMousePressed = new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent event) {
+            rect.setCursor(Cursor.CLOSED_HAND);
+            rect.setLayoutX(amplitude);
+
+        }
+    };
+    EventHandler<MouseEvent> eventMouseDragged = new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent event) {
+            amplitude = event.getSceneX() - rect.getWidth() / 2;
+                rect.setLayoutX(amplitude);
+
+        }
+    };
+    EventHandler<MouseEvent> eventMouseReleased = new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent event) {
+           rect.setCursor(Cursor.OPEN_HAND);
+        }
+    };
+    EventHandler<MouseEvent> eventMouseEnteredTarget = new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent event) {
+           rect.setCursor(Cursor.OPEN_HAND);
+        }
+    };
     /**
      *
      * @param location
@@ -57,7 +90,11 @@ public class SimulationSHMController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        final double initialPosition = 300.0;
+        Group lines = new Group(linePathNegative, lineMiddle, linePathPositive);
+        lines.setVisible(false);
+        resetbtn.setVisible(false);
+        
         /**
          * Change speed of animation
          */
@@ -105,10 +142,19 @@ public class SimulationSHMController implements Initializable {
             } else {
                 shm.getShm().play();
             }
+            
             playbtn.setDisable(true);
             pausebtn.setDisable(false);
             stopbtn.setDisable(false);
             frictionslider.setDisable(true);
+            resetbtn.setVisible(false);
+            
+            rect.cursorProperty().set(Cursor.DEFAULT);
+            rect.removeEventHandler(MouseEvent.MOUSE_ENTERED_TARGET, eventMouseEnteredTarget);
+            rect.removeEventHandler(MouseEvent.MOUSE_PRESSED, eventMousePressed);
+            rect.removeEventHandler(MouseEvent.MOUSE_DRAGGED, eventMouseDragged);
+            rect.removeEventHandler(MouseEvent.MOUSE_RELEASED, eventMouseReleased);
+            
             System.out.println("When play is clicked playbtn is " + playbtn.isDisable() + " pausebtn is " + pausebtn.isDisable() + " stopbtn is " + stopbtn.isDisable());
         });
 
@@ -120,6 +166,7 @@ public class SimulationSHMController implements Initializable {
             playbtn.setDisable(false);
             pausebtn.setDisable(true);
             stopbtn.setDisable(false);
+            resetbtn.setVisible(false);
             System.out.println("When pause is clicked playbtn is " + playbtn.isDisable() + " pausebtn is " + pausebtn.isDisable() + " stopbtn is " + stopbtn.isDisable());
         });
 
@@ -132,6 +179,7 @@ public class SimulationSHMController implements Initializable {
             pausebtn.setDisable(true);
             stopbtn.setDisable(true);
             frictionslider.setDisable(false);
+            resetbtn.setVisible(true);
             System.out.println("When stop is clicked playbtn is " + playbtn.isDisable() + " pausebtn is " + pausebtn.isDisable() + " stopbtn is " + stopbtn.isDisable());
             clickAndDrag();
         });
@@ -142,30 +190,15 @@ public class SimulationSHMController implements Initializable {
         graphbtn.setOnAction((e) -> {
             GraphGenerator graph = new GraphGenerator();
         });
+        
+        resetbtn.setOnAction((e) -> {
+            rect.getTransforms().clear();
+        });
     }
 
     private void clickAndDrag() {
-    EventHandler<MouseEvent> eventMousePressed = new EventHandler<MouseEvent>() {
-
-        @Override
-        public void handle(MouseEvent event) {
-            rect.setCursor(Cursor.CLOSED_HAND);
-            rect.setLayoutX(amplitude);
-
-        }
-    };
-    EventHandler<MouseEvent> eventMouseDragged = new EventHandler<MouseEvent>() {
-
-        @Override
-        public void handle(MouseEvent event) {
-            amplitude = event.getSceneX() - rect.getWidth() / 2;
-                rect.setLayoutX(amplitude);
-
-        }
-    };
-    
-        
-            rect.setCursor(Cursor.OPEN_HAND);
+            
+            rect.addEventHandler(MouseEvent.MOUSE_ENTERED_TARGET, eventMouseEnteredTarget);
             
             //When its closed and pressed make it drags
             //Adding 2 eventHandlers here
@@ -174,17 +207,8 @@ public class SimulationSHMController implements Initializable {
             rect.addEventHandler(MouseEvent.MOUSE_DRAGGED, eventMouseDragged);
             
             //Animate the position of the rectangle to the position of the mouse when released
-            rect.addEventHandler(MouseEvent.MOUSE_RELEASED, (e) -> {
-                
-                    rect.removeEventHandler(MouseEvent.MOUSE_PRESSED, eventMousePressed);
-                    rect.removeEventHandler(MouseEvent.MOUSE_DRAGGED, eventMouseDragged);
-                
-                rect.setCursor(Cursor.DEFAULT);
-                
-                //if playbtn is still of playable tho, don't remove yet
-            });
+            rect.addEventHandler(MouseEvent.MOUSE_RELEASED, eventMouseReleased);
     }
-     //boolean enableClickAndDrag = true;
     public Duration getTime() {
         SimulationSHM simulation = new SimulationSHM(rect, amplitude, originalDuration);
         Duration Time = simulation.getShm().getCurrentTime();
