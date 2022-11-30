5e91ea631e0a9aea2f53ce6ac5663ea6002b7df6
Junior Boni <2158914@edu.vaniercollege.qc.ca>
2022-11-30 15:07:56 -0500

    Updated projectile motion

--- ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/functions/SimulationProjectileMotion.java
+++ ultimate-epic-physic-simulation/app/src/main/java/edu/vanier/ueps/simulations/functions/SimulationProjectileMotion.java
@@ -21,11 +21,11 @@ import javafx.util.Duration;
  */
 public class SimulationProjectileMotion{
         //canvas
-        final Canvas canvas = new Canvas(600, 700);      
+        final Canvas canvas = new Canvas(1300, 500);      
         
         //initial position of rect/shape
-        double initialPosX = 100;
-        double initialPosY = 100;
+        double initialPosX = 150;
+        double initialPosY = 500;
         
         //following position after a certain time    
         double nextPositionX;
@@ -35,7 +35,7 @@ public class SimulationProjectileMotion{
         double time;
         
          //time passed b4 Modification("update")
-        double timeInSeconds;
+        double timeInSeconds = Math.pow(time, -3);
         
         //norm of the velocity vector; more like speed
         double speed;
@@ -49,48 +49,56 @@ public class SimulationProjectileMotion{
         return this.canvas;
     }
 
+    /**
+     * 
+     * @param time
+     * @param speed
+     * @param direction 
+     */
     public SimulationProjectileMotion(double time, double speed, double direction) {
         this.time = time;
-        this.speed = speed;
+        this.speed = speed*Math.pow(10,-3);
         this.direction = direction;
         this.gc = this.canvas.getGraphicsContext2D();
-
-        
     }
         
     public void draw(GraphicsContext gc){
-        gc.fillRect(initialPosX, initialPosY, 50, 50);
-        
+        gc.fillRect(initialPosX, initialPosY, 150, 150);
     }
     
     public void update(){
-        this.initialPosX += projectileMotionX(this.time, this.initialPosX, velocityX(this.speed));
-        this.initialPosY += projectileMotionY(this.time, this.initialPosY, velocityY(this.speed));
+        this.initialPosX = projectileMotionX(this.time, this.initialPosX, velocityX(this.speed));
+        this.initialPosY = projectileMotionY(this.time, this.initialPosY, velocityY(this.speed));
     }
 
     private double velocityX(double speed){
-        //use the fact that cos(direction) = x^2/v^2 -> x = sqrt(cos(direction)/v^2)
-        double velocityX = Math.sqrt(Math.cos(this.direction)/(speed*speed));
+        //use the fact that cos(direction) = x/v -> x = cos(direction)*v)
+        double velocityX = speed*Math.cos(this.direction);
+        //System.out.println("velocity x = " + velocityX);
         return velocityX;
     }
     
     private double velocityY(double speed) {
-        //use the fact that tan(direction) = y^2/v^2 -> x = sqrt(tan(direction)/v^2)
-        double velocityY = Math.sqrt(Math.tan(this.direction)/(speed*speed));
+        //use the fact that tan(direction) = y/v -> y = v*tan(direction))
+        double velocityY = speed*Math.tan(this.direction);
+        //System.out.println("velocity y = " + velocityY);
         return velocityY;
     }
     
     private double projectileMotionX(double time, double currentPositionX, double velocityX){
-        //x position is not affected by gravitational acceleration, it follows a velocity
+        //x position IS NOT affected by gravitational acceleration, it follows a velocity
         double derivedNextX = currentPositionX + time*velocityX;
+        System.out.println("position X update = " + derivedNextX);
         return derivedNextX;
-                
     }
     
     private double projectileMotionY(double time, double currentPositionY, double velocityY){
-        //y position is affected by gravitational constant 
+        //y position IS affected by gravitational constant 
         //yo + voy t ? 4.9 t^2
+        //TODO: fix position Y
+        //Make that it depends on the vector of the projectile; 2 timing: when it goes up, and when it goes down
         double derivedNextY = currentPositionY + time*velocityY - (4.9 * time * time);
+        System.out.println("position Y update = " + derivedNextY);
         return derivedNextY;
     }
     
@@ -98,7 +106,7 @@ public class SimulationProjectileMotion{
         this.gc.setFill(Color.BLUE);
         EventHandler<ActionEvent> onFinished = this::handleAnimationUpdate;
         Timeline animation = new Timeline(new KeyFrame(
-                Duration.millis(50), onFinished)
+                Duration.millis(time), onFinished)
         );
         animation.setCycleCount(Animation.INDEFINITE);
         animation.setAutoReverse(true);
@@ -111,7 +119,7 @@ public class SimulationProjectileMotion{
         if (initialPosY < 0) {
             initialPosY = 0;
         }
-        if (initialPosX > 100) {
+        if (initialPosX > 1300) {
             initialPosX = 0;
         }
         this.gc.clearRect(0, 0, this.canvas.getWidth(), this.canvas.getHeight());
